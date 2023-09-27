97e215356651b6a41e67933d8570a0438a0bfa49
augustocparaujo <augustocparaujo@gmail.com>
2023-09-26 21:46:52 -0300

    wip - organização de rotas

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,7 +12,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        \App\Models\User::factory(10)->create();
+        //se for fazer de cliente criar a faceda em factories
 
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
